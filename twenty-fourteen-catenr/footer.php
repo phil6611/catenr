284da0d703edc69b39f0a75c14eae1c5a5eba7cf
Philippe Poisse <perpinux@localhost>
2017-05-16 13:23:02 +0200

    initial commit

---
+++ twenty-fourteen-catenr/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+
+		<footer id="pied">
+
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="pied-info">
+				<p>06 31 49 27 73</p>
+				<p>contact@catenr.org</p>
+				<p>Suivez nous <img src="http://catenr.org/wp-content/uploads/2017/05/2Mesa-de-trabajo-1.png" alt="Facebook" /> <img src="http://catenr.org/wp-content/uploads/2017/05/3Mesa-de-trabajo-1.png" alt="Twitter" /></p>
+			</div>
+
+		</footer>
+
+	</div><!-- #page -->
+<!-- wp_footer -->
+	<?php wp_footer(); ?>
+</body>
+</html>
